fb955ba531944795265ebc9853b938d7b54a7b89
Ray Wang <apprentice@giantcroissant.com>
2014-10-29 10:04:52 +0800

    Add examples for showing coroutines
    Untrack some files.
    Show two examples for using coroutines.

---
+++ Assets/Scripts/ButtonAppearOneByOne.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.UI;
+
+public class ButtonAppearOneByOne : MonoBehaviour
+{
+	public List<Button> buttons;
+	//private int _index;
+
+	void Start()
+	{
+		StartCoroutine(ShowOneByOne());
+	}
+
+	private IEnumerator ShowOneByOne()
+	{
+		for (int i = 0; i < buttons.Count; ++ i)
+		{
+			buttons[i].gameObject.SetActive(true);
+			yield return new WaitForSeconds(1.0f);
+		}
+
+		StartCoroutine(HideOneByOne());
+	}
+
+	private IEnumerator HideOneByOne()
+	{
+		for (int i = 0; i < buttons.Count; ++ i)
+		{
+			buttons[i].gameObject.SetActive(false);
+			yield return new WaitForSeconds(1.0f);
+		}
+
+		StartCoroutine(ShowOneByOne());
+	}
+}
---
+++ Assets/Scripts/ChainYield.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ChainYield : MonoBehaviour
+{
+	public GameObject stateUI1;
+	public GameObject stateUI2;
+	public GameObject stateUI3;
+
+	void Start()
+	{
+	}
+
+	public void ActivateStates()
+	{
+		StartCoroutine(StartState());
+	}
+
+	private IEnumerator StartState()
+	{
+		yield return StartCoroutine(LinedState1());
+		yield return StartCoroutine(LinedState2());
+		yield return StartCoroutine(LinedState3());
+	}
+
+	private IEnumerator LinedState1()
+	{
+		yield return new WaitForSeconds(1.0f);
+
+		if (stateUI1 != null)
+		{
+			stateUI1.SetActive(true);
+		}
+
+		yield return new WaitForSeconds(1.0f);
+	}
+
+	private IEnumerator LinedState2()
+	{
+		yield return new WaitForSeconds(1.0f);
+		
+		if (stateUI2 != null)
+		{
+			stateUI2.SetActive(true);
+		}
+		
+		yield return new WaitForSeconds(1.0f);
+	}
+
+	private IEnumerator LinedState3()
+	{
+		yield return new WaitForSeconds(1.0f);
+		
+		if (stateUI3 != null)
+		{
+			stateUI3.SetActive(true);
+		}
+		
+		yield return new WaitForSeconds(1.0f);
+	}
+}
